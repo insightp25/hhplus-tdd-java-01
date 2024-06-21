6aa730ffd5bf49129fd8fe065dcf3d0fab0854a3
JO <71416000+insightp25@users.noreply.github.com>
2024-06-21 12:21:39 +0900

    service 동시성 제어 및 typo 수정

--- src/main/java/io/hhplus/tdd/controller/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/controller/ApiControllerAdvice.java
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.controller;
 
 import io.hhplus.tdd.controller.response.ErrorResponse;
+import io.hhplus.tdd.domain.exception.BadInputPointValueException;
 import io.hhplus.tdd.domain.exception.InsufficientPointsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,8 +20,8 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(409).body(new ErrorResponse("409", "포인트 잔액이 부족합니다."));
     }
 
-    @ExceptionHandler(value = InsufficientPointsException.class)
-    public ResponseEntity<ErrorResponse> InvalidPointsException(Exception e) {
-        return ResponseEntity.status(400).body(new ErrorResponse("400", "잘못된 포인트 입력입니다."));
+    @ExceptionHandler(value = BadInputPointValueException.class)
+    public ResponseEntity<ErrorResponse> handleBadInputPointValueException(Exception e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", "잘못된 포인트 입력입니다. 입력은 양의 정수로만 가능합니다."));
     }
 }
